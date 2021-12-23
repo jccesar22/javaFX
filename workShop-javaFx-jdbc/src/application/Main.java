477e32cd86c855e9ccb931b161e1edb9c36fb584
Júlio César Soares de Almeida <60848753+jccesar22@users.noreply.github.com>
2021-12-22 23:30:33 -0300

    commit

--- workShop-javaFx-jdbc/src/application/Main.java
+++ workShop-javaFx-jdbc/src/application/Main.java
@@ -9,8 +9,10 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	private static Scene mainScene;
 	@Override
 	public void start(Stage primaryStage) {
+		
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
@@ -18,7 +20,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);//isso vai permitir para que o scrollPane fique do tamanho do pai
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -27,6 +29,10 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+		
+	}
 	public static void main(String[] args) {
 		launch(args);
 	}
--- workShop-javaFx-jdbc/src/gui/MainViewController.java
+++ workShop-javaFx-jdbc/src/gui/MainViewController.java
@@ -1,12 +1,17 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 	//inicializando as varriavel dos menu criado na MainView
@@ -40,7 +45,16 @@ public class MainViewController implements Initializable{
 	}
 	
 	private void LoadVew(String absoluteName) {
+		try {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+		VBox newVBox = loader.load();
+		
+		
+		}
+		catch(IOException e) {
+			Alerts.showAlert("Io Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+		
 	}
 		
 }
