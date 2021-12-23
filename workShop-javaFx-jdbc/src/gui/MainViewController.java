d8d81ac54b1595a147e80be6800526e1db651329
Júlio César Soares de Almeida <60848753+jccesar22@users.noreply.github.com>
2021-12-23 09:31:49 -0300

    commit
    fazendo a Class MainViewwController

--- workShop-javaFx-jdbc/src/gui/MainViewController.java
+++ workShop-javaFx-jdbc/src/gui/MainViewController.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
@@ -35,7 +38,7 @@ public class MainViewController implements Initializable{
 	}
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		LoadView("/gui/About.fxml");
 	}
 
 	@Override
@@ -44,11 +47,18 @@ public class MainViewController implements Initializable{
 		
 	}
 	
-	private void LoadVew(String absoluteName) {
+	private synchronized void LoadView(String absoluteName) {
 		try {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 		VBox newVBox = loader.load();
 		
+		Scene mainScene = Main.getMainScene();
+		VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();//fazendo cast para poder acessar o mainBox principal
+		  
+		Node mainMMenu = mainVBox.getChildren().get(0);
+		mainVBox.getChildren().clear();
+		mainVBox.getChildren().add(mainMMenu);
+		mainVBox.getChildren().addAll(newVBox.getChildren());
 		
 		}
 		catch(IOException e) {
